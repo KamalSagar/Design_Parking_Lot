b9515c6b61b9374a35b30287956ef974a4d077f4
Kamal Sagar <kamalsagar316@gmail.com>
2024-10-22 20:54:28 +0530

    Initialized the Parking lot project

---
+++ src/main/java/dev/kamal/design_parking_lot/DesignParkingLotApplication.java
@@ -0,0 +1,13 @@
+package dev.kamal.design_parking_lot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignParkingLotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignParkingLotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/kamal/design_parking_lot/DesignParkingLotApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kamal.design_parking_lot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignParkingLotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
